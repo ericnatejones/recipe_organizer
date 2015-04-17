1d18b7a546a61c8ab3d97f3227af9613c7a9a05e
Alex Lyman <alexjohnlyman@gmail.com>
2015-04-17 14:52:44 -0600

    Added edit functionality for recipes

--- frontend/app/recipe-detail/recipeDetail.js
+++ frontend/app/recipe-detail/recipeDetail.js
@@ -12,6 +12,8 @@ angular.module('myApp.recipeDetail', ['ngRoute'])
     .controller('RecipeDetailCtrl', ['$scope', '$routeParams', '$location', 'Restangular', function ($scope, $routeParams, $location, Restangular) {
         $scope.recipeId = $routeParams.recipeId;
 
+        $scope.editing = false;
+
         Restangular.one('recipes', $scope.recipeId).customGET().then(function (recipe) {
             $scope.recipe = recipe;
         });
@@ -33,12 +35,21 @@ angular.module('myApp.recipeDetail', ['ngRoute'])
             var confirmation = confirm("Are you sure that you want to go back to the recipe list?");
             if(confirmation) {
                     $location.path('/recipes')
-            };
+            }
         };
         $scope.addRecipe = function() {
             var confirmation = confirm("Let's add a new recipe!");
             if(confirmation) {
                     $location.path('/add-recipe')
             };
+        };
+
+        $scope.saveEditedRecipe = function () {
+            Restangular.one('recipes', $scope.recipeId).customPUT($scope.recipe).then(function () {
+                alert("Your recipe was successfully updated!");
+                $scope.editing = false;
+            }, function() {
+                alert("Something went wrong updating the recipe...");
+            });
         };
     }]);
