78f317fa5b046ffccf19554a45d5b7fa2acc7d2b
Alex Lyman <alexjohnlyman@gmail.com>
2015-05-27 14:00:01 -0600

    Additional adjustments on the index.html. Started creating test files.

---
+++ frontend/app/recipes/recipes_test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('myApp.recipes module', function () {
+
+    beforeEach(module('myApp.recipes'));
+
+    describe('recipes controller', function () {
+
+        it('should ....', inject(function ($controller) {
+            var RecipesCtrl = $controller('RecipesCtrl');
+            expect(RecipesCtrl).toBeDefined();
+        }));
+
+        //check for a response
+        it('should ....', inject(function ($scope.addRecipe) {
+            var $scope.addRecipe = $controller();
+            expect(addRecipe).toBeDefined();
+        }));
+
+
+    });
+});
+
